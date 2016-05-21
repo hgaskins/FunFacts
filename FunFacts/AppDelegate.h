6ddd2f62f20eba01e4a6f56e0ae4ffd097dc15fe
Hannah Gaskins <gaskinshannah@gmail.com>
2016-05-21 09:12:54 -0700

    Initial Commit

---
+++ FunFacts/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FunFacts
+//
+//  Created by hannah gaskins on 5/21/16.
+//  Copyright © 2016 hannah gaskins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FunFacts/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FunFacts
+//
+//  Created by hannah gaskins on 5/21/16.
+//  Copyright © 2016 hannah gaskins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
